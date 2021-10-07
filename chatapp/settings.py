4a7506bae9271525ef1573e64d0f33cce4a636b8
Arpan Sharma <90550606+arpansharma0016@users.noreply.github.com>
2021-10-07 20:22:54 +0530

    Update settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9sem32#f*0m*n^+yy8g($pkj#3lj4hhb9099!j421ta#t0+a8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
